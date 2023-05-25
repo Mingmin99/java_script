2add7e8eb2b1743ffb144a4023c5e186a5d4bb7b
minyoung <alsdud11737@naver.com>
2023-05-25 11:26:24 +0900

    for2

--- javascript/arr01.js
+++
@@ -1,9 +0,0 @@
-let str = "광명폴리텍대학교";
-console.log(str);
-console.log("문자열의 길이는=" + str.length);
-
-console.log(str[0]);
-
-for (let i = 0; i < str.length; i++) {
-    console.log(str[i])
-}
--- javascript/arr03.js
+++
@@ -1,9 +0,0 @@
-let fruits = ['사과', " 배", '오렌지', "바나나"];
-console.log(fruits);
-
-let result = fruits.splice(2,1);
-console.log(result);
-console.log(fruits);
-console.log("--------------");
-fruits.push(result[0]);
-console.log(fruits);

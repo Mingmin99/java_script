454174bbf35af84502a52f180a1d508c631fa20b
minyoung <alsdud11737@naver.com>
2023-05-25 15:37:06 +0900

    dom

---
+++ src/javascript/function01.js
@@ -0,0 +1,13 @@
+//선언
+function hello(){
+    console.log("안녕하세요.")
+}
+//호출
+hello();
+
+
+let greeting = function(){
+    console.log("반갑습니다.")
+}
+
+greeting();
---
+++ src/javascript/function04.js
@@ -0,0 +1,7 @@
+var add = new Function("x,y", "return x+y")
+console.log(add(1,2));
+
+function plus(x,y){
+    return x+y;
+}
+console.log(plus(1,2));
